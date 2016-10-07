20f6e1a1e407d059e3a4dc15e8b9af692c475dac
Wesley Van Malcot <wesley.vanmalcot@hotmail.com>
2016-10-07 15:37:08 +0200

    Corrected printout

--- src/main/java/com/realdolmen/gitex1/App.java
+++ src/main/java/com/realdolmen/gitex1/App.java
@@ -17,14 +17,14 @@ public class App
         System.out.println("Reversing the var values without creating a third variable..");
         
         reverseNumericVarValue(firstNumber, secondNumber);
-        
-        System.out.println( "My new value for var firstNumber is: " + firstNumber);
-        System.out.println( "My new value for var secondNumber is: " + secondNumber);
     }
 
 	private static void reverseNumericVarValue(int firstNumber, int secondNumber) {
 		firstNumber = firstNumber + secondNumber;
 		secondNumber = firstNumber - secondNumber;
 		firstNumber = secondNumber - firstNumber;
+        
+        	System.out.println( "My new value for var firstNumber is: " + firstNumber);
+        	System.out.println( "My new value for var secondNumber is: " + secondNumber);
 	}
 }
