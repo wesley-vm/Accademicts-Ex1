9e316802edecf8b221193a8b22c8297b187b5f92
Melvin Hulsmans <Melvin.Hulsmans@gmail.com>
2016-10-07 15:50:02 +0200

    modify logic

--- src/main/java/com/realdolmen/gitex1/App.java
+++ src/main/java/com/realdolmen/gitex1/App.java
@@ -6,25 +6,28 @@ package com.realdolmen.gitex1;
  */
 public class App 
 {
+	private static int firstNumber;
+	private static int secondNumber;
+	
     public static void main( String[] args )
     {
-    	int firstNumber = 10;
-    	int secondNumber = 5;
+    	firstNumber = 10;
+    	secondNumber = 5;
     	
         System.out.println( "My original value for var firstNumber is: " + firstNumber);
         System.out.println( "My original value for var secondNumber is: " + secondNumber);
         
         System.out.println("Reversing the var values without creating a third variable..");
         
-        reverseNumericVarValue(firstNumber, secondNumber);
+        reverseNumericVarValue();
         
         System.out.println( "My new value for var firstNumber is: " + firstNumber);
         System.out.println( "My new value for var secondNumber is: " + secondNumber);
     }
 
-	private static void reverseNumericVarValue(int firstNumber, int secondNumber) {
+	private static void reverseNumericVarValue() {
 		firstNumber = firstNumber + secondNumber;
 		secondNumber = firstNumber - secondNumber;
-		firstNumber = secondNumber - firstNumber;
+		firstNumber = firstNumber - secondNumber;
 	}
 }
